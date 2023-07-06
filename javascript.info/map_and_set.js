c16b9198a76b8b3192e777a4af17b5ae211bc11d
masterofcsharp <109514731+masterofcsharp@users.noreply.github.com>
2023-07-06 10:12:55 +0000

    m

--- javascript.info/map_and_set.js
+++ javascript.info/map_and_set.js
@@ -36,3 +36,25 @@ map.set('1', 'str1')
   .set(1, 'num1')
   .set(true, 'bool1');
 
+let recipeMap = new Map([
+    ['cucumber', 500],
+    ['tomatoes', 350],
+    ['onion', 50]
+]);
+
+// iterate over keys (vegetables)
+for (let vegetable of recipeMap.keys()) {
+    alert(vegetable); // cucumber, tomatoes, onion
+}
+
+// iterate over values (amounts)
+for (let amount of recipeMap.values()) {
+    alert(amount); // 500, 350, 50
+}
+
+// iterate over [key, value] entries
+for (let entry of recipeMap) { // the same as of recipeMap.entries()
+    alert(entry); // cucumber, 500 (and so on)
+}
+
+
