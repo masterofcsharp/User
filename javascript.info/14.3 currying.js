34dd05bf04781956c191e396622e973843ba1953
masterofcsharp <109514731+masterofcsharp@users.noreply.github.com>
2023-07-06 17:22:05 +0000

    curry

--- javascript.info/14.3 currying.js
+++ javascript.info/14.3 currying.js
@@ -27,3 +27,43 @@ alert( curriedSum(1)(2) ); // 3, called partially
 function log(date, importance, message) {
   alert(`[${date.getHours()}:${date.getMinutes()}] [${importance}] ${message}`);
 }
+
+log = _.curry(log);
+
+log(new Date(), "DEBUG", "some debug"); // log(a, b, c)
+
+log(new Date())("DEBUG")("some debug"); // log(a)(b)(c)
+
+// logNow will be the partial of log with fixed first argument
+let logNow = log(new Date());
+
+// use it
+logNow("INFO", "message"); // [HH:mm] INFO message
+
+let debugNow = logNow("DEBUG");
+
+debugNow("message"); // [HH:mm] DEBUG message
+
+function curry(func) {
+
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function(...args2) {
+        return curried.apply(this, args.concat(args2));
+      }
+    }
+  };
+
+}
+
+function sum(a, b, c) {
+  return a + b + c;
+}
+
+let curriedSum = curry(sum);
+
+alert( curriedSum(1, 2, 3) ); // 6, still callable normally
+alert( curriedSum(1)(2,3) ); // 6, currying of 1st arg
+alert( curriedSum(1)(2)(3) ); // 6, full currying
